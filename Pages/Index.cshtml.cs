0b50474537cfc4ff7a286527b59d0fe6e0284c48
Orchiid <joannatolofari@gmail.com>
2023-12-08 16:26:33 +0100

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StaffPortal.Pages
+{
+    public class IndexModel : PageModel
+    {
+       
+        }
+}
